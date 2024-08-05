300b4eda15fb9d54847fedf92bfd75577e3506ef
Suhail Ahmad <suhailahmad9091@gmail.com>
2024-08-05 18:15:12 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'Lamborghini',
+        path: 'musics/Lamborghini.mp3',
+        artist: 'Ragini',
+        cover: 'images/Lamborghini.jpg'
+    },
+    {
+        name: 'Shopping',
+        path: 'musics/Shopping.mp3',
+        artist: 'Jass Manak',
+        cover: 'images/Shopping.jpg'
+    },
+    {
+        name: 'Sanjog',
+        path: 'musics/Sanjog.mp3',
+        artist: 'Mehtab Virik',
+        cover: 'images/Sanjog.jpg'
+    },
+    {
+        name: 'Beliya',
+        path: 'musics/Beliya.mp3',
+        artist: 'Gurnam Bhullar',
+        cover: 'images/Beliya.jpg'
+    },
+    {
+        name: 'Lehanga',
+        path: 'musics/Lehanga.mp3',
+        artist: 'Jass Manak',
+        cover: 'images/Lehanga.jpg'
+    },
+]
